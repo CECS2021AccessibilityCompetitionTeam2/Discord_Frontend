98a5991166d7f4852416dc22333d7c121fc4f6cd
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2021-01-30 12:10:32 -0800

    Discord respond back and fix merge conflict

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const yaml = require("js-yaml");
 const fs = require("fs");
 
-// const auth = require('./token.json');
 const client = new Discord.Client();
 
 var prefix = "z!";
@@ -13,18 +12,35 @@ try {
   console.log(e);
 }
 
+function introMessage() {
+  console.log(`
+  *** This Bot is ready to play! ***
+  !listen -> join the voice chanel
+  `);
+}
+
 //Important (initializes bot)
-client.once("ready", () => {
-  console.log("Ready!");
-});
+client.once("ready", introMessage.bind(this));
 
 const regCMD = new RegExp(prefix + "(.*)");
 
-client.on("message", (msg) => {
-  if (regCMD.test(msg)) {
-    const match = msg.content.match(regCMD); //array of regex match
-  } else {
-    console.log(msg.author.username + ": " + msg);
-    console.log("🚀");
+
+client.on("message", discordMessage.bind(this));
+
+function discordMessage(msg){
+  let discordCommand = msg.content.toLowerCase().slice(1);
+  switch (discordCommand) {
+    case 'hello' : 
+      msg.reply(`Hello ${msg.author.username}`);
+      break;
+    case 'listen' : 
+      textChannel = msg.channel;
+      msg.reply(`Going to listen to voice channel`);
+      listenToVoiceChannel(msg);
   }
-});
+}
+
+function listenToVoiceChannel(voice) {
+  let voiceMember = voice.member;
+  let listening = true;
+}
