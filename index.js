12147de18ec40a87f3f0ea44a4a7ce08aa86f9fa
Adwitiya Singh <adwitiya.karan@gmail.com>
2021-01-29 21:54:53 -0800

    Use and Add prettier as a depenency

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const Discord = require('discord.js');
-const yaml = require('js-yaml');
-const fs = require('fs');
+const Discord = require("discord.js");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 // const auth = require('./token.json');
 const client = new Discord.Client();
 
-
 var prefix = "z!";
 try {
-    const token = yaml.load(fs.readFileSync('./token.yaml', "UTF-8"));
-    client.login(token.token);
+  const token = yaml.load(fs.readFileSync("./token.yaml", "UTF-8"));
+  client.login(token.token);
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }
 
 //Important (initializes bot)
-client.once('ready', () => {
-    console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
-
 
 const regCMD = new RegExp(prefix + "(.*)");
 
-client.on('message', msg => {
-    if (regCMD.test(msg)) {
-        const match = msg.content.match(regCMD);//array of regex match
-    }
-    else {
-        console.log(msg.author.username + ": " + msg);
-        console.log("🚀");
-    }
+client.on("message", (msg) => {
+  if (regCMD.test(msg)) {
+    const match = msg.content.match(regCMD); //array of regex match
+  } else {
+    console.log(msg.author.username + ": " + msg);
+    console.log("🚀");
+  }
 });
