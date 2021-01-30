c78bc43e2c6a9f3ab2f3fcffb84c5f16c18badcf
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2021-01-30 12:20:42 -0800

    Update credentials for our project. Using source from https://github.com/erazavi/AccessibilityCodingCompetition2020 Begin working on voice channel detection

--- index.js
+++ index.js
@@ -1,30 +1,143 @@
-const Discord = require("discord.js");
+require('dotenv').config()
+const Discord = require('discord.js');
 const yaml = require("js-yaml");
 const fs = require("fs");
-
-// const auth = require('./token.json');
 const client = new Discord.Client();
 
-var prefix = "z!";
-try {
-  const token = yaml.load(fs.readFileSync("./token.yaml", "UTF-8"));
-  client.login(token.token);
-} catch (e) {
-  console.log(e);
+const { Readable } = require('stream');
+
+const SILENCE_FRAME = Buffer.from([0xF8, 0xFF, 0xFE]);
+
+class Silence extends Readable {
+  _read() {
+    this.push(SILENCE_FRAME);
+  }
 }
 
-//Important (initializes bot)
-client.once("ready", () => {
-  console.log("Ready!");
-});
+const { Transform } = require('stream')
 
-const regCMD = new RegExp(prefix + "(.*)");
+function convertBufferTo1Channel(buffer) {
+  const convertedBuffer = Buffer.alloc(buffer.length / 2)
 
-client.on("message", (msg) => {
-  if (regCMD.test(msg)) {
-    const match = msg.content.match(regCMD); //array of regex match
-  } else {
-    console.log(msg.author.username + ": " + msg);
-    console.log("🚀");
+  for (let i = 0; i < convertedBuffer.length / 2; i++) {
+    const uint16 = buffer.readUInt16LE(i * 4)
+    convertedBuffer.writeUInt16LE(uint16, i * 2)
   }
+
+  return convertedBuffer
+}
+
+class ConvertTo1ChannelStream extends Transform {
+  constructor(source, options) {
+    super(options)
+  }
+
+  _transform(data, encoding, next) {
+    next(null, convertBufferTo1Channel(data))
+  }
+}
+
+const googleSpeech = require('@google-cloud/speech')
+
+const googleSpeechClient = new googleSpeech.SpeechClient()
+
+var prefix = "?";
+var followingUser = '';
+var defaultChannel = '';
+
+client.once('ready', () => {
+	console.log('Ready!')
+	client.guilds.cache.forEach(Guild => {
+		let channelID;
+		let channels = Guild.channels.cache;
+		for (let c of channels){
+			let channelType = c[1].type;
+			if (channelType === "text") {
+				channelID = c[0];
+				break;
+			}
+		}
+		defaultChannel = channels.get(Guild.systemChannelID || channelID);
+		//defaultChannel.send("PING PONG I AM ONLINE!");
+	});
 });
+
+client.on('message', msg => {
+	if (msg.content === prefix + 'followme') {
+		followingUser = msg.member.user.id;
+		console.log("Following user: " + followingUser);
+	}
+})
+
+client.on('voiceStateUpdate', async (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel
+  let oldUserChannel = oldMember.voiceChannel
+  console.log("USER VOICE CHANNEL STATE CHANGE!");
+  console.log("user " + oldMember.id);
+  console.log("previous channel: " + oldMember.voiceChannelID);
+  console.log("new channel: " + newMember.voiceChannelID);
+	if (newMember.id === followingUser){
+	  if((oldUserChannel === undefined && newUserChannel !== undefined)
+		|| (oldUserChannel !== undefined && newUserChannel !== undefined && oldUserChannel !== newUserChannel)){
+		 // User Joins a voice channel
+		console.log("Joined " + newUserChannel.id);
+		  const connection = await newMember.voiceChannel.join();
+		  const receiver = connection.createReceiver();
+		  defaultChannel.send("I am listening to you");
+		 	connection.playStream(new Silence(), { type: 'opus' });
+		  connection.on('speaking', (user, speaking) => {
+			if (!speaking) {
+			  return
+			}
+
+			console.log(`I'm listening to ${user.username}`)
+
+			// this creates a 16-bit signed PCM, stereo 48KHz stream
+			const audioStream = receiver.createOpusStream(user)
+			const requestConfig = {
+			  encoding: 'LINEAR16',
+			  sampleRateHertz: 48000,
+			  languageCode: 'en-US'
+			}
+			const request = {
+			  config: requestConfig,
+			  user:user.username
+			}
+			const recognizeStream = googleSpeechClient
+			  .streamingRecognize(request)
+			  .on('error', console.error)
+			  .on('data', response => {
+				const transcription = response.results
+				  .map(result => result.alternatives[0].transcript)
+				  .join('\n')
+				  .toLowerCase()
+				console.log(`Transcription: ${transcription}`)
+			  })
+
+			const convertTo1ChannelStream = new ConvertTo1ChannelStream()
+
+			audioStream.pipe(convertTo1ChannelStream).pipe(recognizeStream)
+
+			audioStream.on('end', async () => {
+			  console.log('audioStream end')
+					})
+		});
+
+	  }else if(newUserChannel === undefined){
+		console.log("User left the channel");
+		oldUserChannel.leave();
+		// User leaves a voice channel
+
+	  }
+	}
+})
+
+try {
+	const token = yaml.load(fs.readFileSync("./token.yaml", "UTF-8"));
+	client.login(token.token);
+  } catch (e) {
+	console.log(e);
+}
+
+
+
